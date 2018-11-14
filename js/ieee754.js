2bc9b7364a8c5058fd48ce4c83f5b8e2e94daef9
Quiquebgit <44301678+Quiquebgit@users.noreply.github.com>
2018-11-14 03:26:27 +0100

    Changes to be committed modified:   js/ieee754.js

--- js/ieee754.js
+++ js/ieee754.js
@@ -1,8 +1,57 @@
 'use strict';
 
 function ieee754aBinario(numero) {
-   numero = "00000000000000000000000000000000";
-   return numero;
+
+	var bitSigno = "0";
+	if(Math.sign(numero) == -1){
+		bitSigno = "1";
+	}
+	numero = numero.replace("-", "");
+	var numeroBinario = pasarABinario(numero);
+	var desplazamientos = numeroBinario.toString().indexOf(".")-1;
+	var exponente = pasarABinario((desplazamientos+127).toString());
+	while (exponente.toString().length < 8) {
+		exponente = "0"+exponente;
+	}
+
+	var bits = numeroBinario.toString().replace(".", "").substring(1).substring(0, 23);
+	while (bits.length < 23) {
+		bits += "0";
+	}
+	console.log("ieee754aBinario: ["+bitSigno+"]["+exponente+"]["+bits+"]");
+	return bitSigno+exponente+bits;
+}
+
+function pasarABinario(numero) {
+	var binario = "";
+	var binarioDecimal = "";
+	var producto;
+
+	if(numero.indexOf(".") != -1){
+		var parteEntera = numero.split(".")[0];
+		var parteDecimal = "0."+numero.split(".")[1];
+
+		do {
+			binario = parteEntera%2+binario;
+			parteEntera /= 2;
+			parteEntera = parseInt(parteEntera);
+		} while (parteEntera/2!=0);
+		while (parteDecimal != 0 && binarioDecimal.toString().length < 24){
+			producto = parteDecimal *= 2;
+			binarioDecimal += producto.toString().split(".")[0];
+			parteDecimal = "0."+producto.toString().split(".")[1];
+			parteDecimal = parseFloat(parteDecimal);
+		}
+		binario = binario+"."+binarioDecimal;
+	} else {
+		do {
+			binario = numero%2+binario;
+			numero /= 2;
+			numero = parseInt(numero);
+		} while (numero/2!=0);
+	}
+	console.log("Número binario: "+binario);
+	return binario;
 }
 
 
