a44e4f876170c8b2c887af0b4964a8bdfe5e0bc9
Quiquebgit <elquique.bg@gmail.com>
2018-12-10 01:06:38 +0100

    Correcciones en el archivo ieee754.js

--- js/ieee754.js
+++ js/ieee754.js
@@ -2,23 +2,32 @@
 
 function ieee754aBinario(numero) {
 
+	numero = numero.toString();
 	var bitSigno = "0";
-	if(Math.sign(numero) == -1){
+	if(Math.sign(numero) == -1 || numero.substring(0,1)=="-"){
 		bitSigno = "1";
 	}
 	numero = numero.replace("-", "");
 	var numeroBinario = pasarABinario(numero);
-	if(numeroBinario.toString().indexOf(".") != -1){
-		var desplazamientos = numeroBinario.toString().indexOf(".")-1;
+	if(numeroBinario != "0"){
+		if(numeroBinario.indexOf(".") != -1){
+			var desplazamientos = numeroBinario.indexOf(".")-1;
+		} else {
+			var desplazamientos = numeroBinario.length-1;
+		}
+		while(numeroBinario.substring(0,1)=="0"){
+			desplazamientos--;
+			numeroBinario = numeroBinario.substring(2,3)+"."+numeroBinario.substring(3);
+		}
+		var exponente = pasarABinario((desplazamientos+127).toString());
 	} else {
-		var desplazamientos = numeroBinario.toString().length-1;
+		exponente = "0"
 	}
-	var exponente = pasarABinario((desplazamientos+127).toString());
-	while (exponente.toString().length < 8) {
+	while (exponente.length < 8) {
 		exponente = "0"+exponente;
 	}
 
-	var bits = numeroBinario.toString().replace(".", "").substring(1).substring(0, 23);
+	var bits = numeroBinario.replace(".", "").substring(1).substring(0, 23);
 	while (bits.length < 23) {
 		bits += "0";
 	}
@@ -35,11 +44,11 @@ function pasarABinario(numero) {
 		var parteEntera = numero.split(".")[0];
 		var parteDecimal = "0."+numero.split(".")[1];
 
-		do {
+		while (parteEntera/2!=0) {
 			binario = parteEntera%2+binario;
 			parteEntera /= 2;
 			parteEntera = parseInt(parteEntera);
-		} while (parteEntera/2!=0);
+		} 
 		while (parteDecimal != 0 && binarioDecimal.toString().length < 24){
 			producto = parteDecimal *= 2;
 			binarioDecimal += producto.toString().split(".")[0];
@@ -48,11 +57,17 @@ function pasarABinario(numero) {
 		}
 		binario = binario+"."+binarioDecimal;
 	} else {
-		do {
+		while (numero/2!=0) {
 			binario = numero%2+binario;
 			numero /= 2;
 			numero = parseInt(numero);
-		} while (numero/2!=0);
+		}
+	}
+	binario = binario.toString();
+	if (binario.substring(0,1)=="."){
+		binario = "0"+binario;
+	} else if (binario == "") {
+		binario = "0";
 	}
 	console.log("Número binario: "+binario);
 	return binario;
